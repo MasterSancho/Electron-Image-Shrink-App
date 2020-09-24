d86bbddc9ee9f64d9eb3749cad0d918dd8dcb981
Alex M <57605252+MasterSancho@users.noreply.github.com>
2020-09-24 21:26:08 +0300

    004-Part 4
    -Menu & Global Shortcuts

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow, Menu } = require('electron');
+const { app, BrowserWindow, Menu, globalShortcut } = require('electron');
 
 // Set env
 process.env.NODE_ENV = 'development';
@@ -13,8 +13,9 @@ function createMainWindow() {
         title: 'ImageShrink',
         width: 500,
         height: 600,
-        icon: `${__dirname}/assets/icons/Icon_256x256.png`,
-        resizable: isDev,
+        icon: './assets/icons/Icon_256x256.png',
+        resizable: isDev ? true : false,
+        backgroundColor: 'white',
     });
 
     mainWindow.loadFile('./app/index.html');
@@ -26,6 +27,11 @@ app.on('ready', () => {
     const mainMenu = Menu.buildFromTemplate(menu);
     Menu.setApplicationMenu(mainMenu);
 
+    globalShortcut.register('CmdOrCtrl+R', () => mainWindow.reload());
+    globalShortcut.register(isMac ? 'Command+Alt+I' : 'Ctrl+Shift+I', () =>
+        mainWindow.toggleDevTools()
+    );
+
     mainWindow.on('closed', () => (mainWindow = null));
 });
 
@@ -36,6 +42,7 @@ const menu = [
         submenu: [
             {
                 label: 'Quit',
+                accelerator: 'CmdOrCtrl+W',
                 click: () => app.quit(),
             },
         ],
